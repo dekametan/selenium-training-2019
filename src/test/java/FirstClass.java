449a7f08ca360b724728ed2cfa2b9682380b655c
slysiuch <serhii.lysiuchenko@globallogic.com>
2019-10-22 09:44:34 +0200

    Minor changes

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,10 +1,6 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -15,11 +11,9 @@ public class FirstClass extends TestBase {
     @Test
     public void testFirst(){
         driver.get("http://demo.litecart.net/admin/");
-
-        Wait<WebDriver> wait = new WebDriverWait(driver, 5).withMessage("Element was not found");
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("button.btn"))));
         driver.findElement(By.cssSelector("button.btn")).click();
 
+        // Time out added to wait for left panel to be completely rendered
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
